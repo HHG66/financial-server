441e58982b70a3418cc265a7bf9e6919a4aac64d
韩宏广 <2421578700@qq.com>
2023-04-17 22:34:28 +0800

    style：修改数据库链接

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,17 +1,17 @@
 /*
  * @Author: HHG
  * @Date: 2023-02-12 11:37:58
- * @LastEditTime: 2023-04-10 16:48:44
+ * @LastEditTime: 2023-04-17 22:31:53
  * @LastEditors: 韩宏广
- * @FilePath: \server\src\lib\db.js
+ * @FilePath: /server/src/lib/db.js
  * @文件说明: 
  */
 const mongoose = require('mongoose');
 const errorMiddlware = require('@/middleware/error.js')
 module.exports = async (resolve, reject) => {
   mongoose.set("strictQuery", false);
-  // await mongoose.connect('mongodb://192.168.0.109:27017/financial', {
-    await mongoose.connect('mongodb://frp-hip.top:13552/financial', {
+  await mongoose.connect('mongodb://192.168.0.101:27017/financial', {
+    // await mongoose.connect('mongodb://frp-hip.top:13552/financial', {
     // await mongoose.connect('mongodb://127.0.0.1:27017/financial', {
     // mongoose.connect('mongodb://mywebsite:han1314.@118.31.79.83:27017/my_website', {
     //mongodb://mywebsite:han1314.@118.31.79.83:27017/my_website
